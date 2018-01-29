1b4bbcf5dceb862b5d1faa121160b04a3445fc51
unknown <rogowski@NB-006-TRI.initions.local>
2018-01-29 11:56:20 +0100

    initial commit for refactoring coding dojo

---
+++ Trivia/RunTheGame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UglyTrivia;
+namespace Trivia
+{
+    class RunTheGame
+    {
+        private static bool notAWinner;
+        public static void Main(String[] args)
+        {
+            Game aGame = new Game();
+
+            aGame.add("Chet");
+            aGame.add("Pat");
+            aGame.add("Sue");
+
+            Random rand = new Random();
+
+            do
+            {
+
+                aGame.roll(rand.Next(5) + 1);
+
+                if (rand.Next(9) == 7)
+                {
+                    notAWinner = aGame.wrongAnswer();
+                }
+                else
+                {
+                    notAWinner = aGame.wasCorrectlyAnswered();
+                }
+
+
+
+            } while (notAWinner);
+
+            Console.WriteLine("game finished");
+            Console.Read();
+        }
+    
+    }
+}
