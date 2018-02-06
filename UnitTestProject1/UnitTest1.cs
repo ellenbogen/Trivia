1ff7b6c0b9c886429a1b9c88b6a851253ce4fd73
unknown <rogowski@NB-006-TRI.initions.local>
2018-02-06 11:56:03 +0100

    some comments

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UglyTrivia;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Game c  = new Game();
+            Assert.IsFalse(c.isPlayable());
+        }
+    }
+}
